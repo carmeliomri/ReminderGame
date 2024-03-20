592291f1db695e7b67b42ec5ad0d1b1e9c0271a8
carmeliomri <carmeli.omri@gmail.com>
2024-03-20 08:26:59 +0200

    card flip function added

--- app/src/main/java/com/example/remindergame/MainActivity.java
+++ app/src/main/java/com/example/remindergame/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.remindergame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void knock(View view) {
+        int id = view.getId();
+        if (id == R.id.beagle1)
+        {
+            ImageView card = (ImageView) view;
+            card.setImageResource(R.drawable.beagle);
+        }
+
     }
 }
