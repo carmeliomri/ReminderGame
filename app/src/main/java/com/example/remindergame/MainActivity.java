3dc4c43e738c334212ec8143d176729f6f1681f5
carmeliomri <carmeli.omri@gmail.com>
2024-05-13 19:03:59 +0300

    game finished

--- app/src/main/java/com/example/remindergame/MainActivity.java
+++ app/src/main/java/com/example/remindergame/MainActivity.java
@@ -1,25 +1,47 @@
 package com.example.remindergame;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private int lastCardId;
+    private ImageView lastCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.lastCardId=-1;
+        this.lastCard=null;
         setContentView(R.layout.activity_main);
     }
-    public void knock(View view) {
-        int id = view.getId();
-        if (id == R.id.beagle1)
-        {
-            ImageView card = (ImageView) view;
-            card.setImageResource(R.drawable.beagle);
-        }
 
+    public void knock(View view){
+        ImageView card = (ImageView) view;
+        if(view.getId()==R.id.pug) card.setImageResource(R.drawable.pug);
+        if(view.getId()==R.id.beagle) card.setImageResource(R.drawable.beagle);
+        if(view.getId()==R.id.french_bulldog) card.setImageResource(R.drawable.french_bulldog);
+        if(view.getId()==R.id.golden_retiriever) card.setImageResource(R.drawable.golden_retriever);
+        //Log.d("test",this.lastCardId+" ");
+        if(this.lastCardId==-1){//first flip
+            this.lastCardId=view.getId();
+            this.lastCard=card;
+        }
+        else {//second flip
+            if (view.getId()!=this.lastCardId) {//second flip - mistake
+                card.setImageResource(R.drawable.back_of_card);
+                lastCard.setImageResource(R.drawable.back_of_card);
+                lastCardId = -1;
+                lastCard = null;
+            } else {//second flip - correct
+                lastCardId = -1;
+                lastCard = null;
+            }
+        }
     }
+
 }
